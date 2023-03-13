84da956f50ab202e883974ce8571a9a9cf237f70
eeue56 <enalicho@gmail.com>
2023-03-13 21:12:30 +0100

    add a test in a sub folder

---
+++ src/__snapshots__/dot_test/snapshotOther.ts
@@ -0,0 +1,1 @@
+export const snapshotOther = 1;
--- src/tests.folder/dot_test.ts
+++ src/tests.folder/dot_test.ts
@@ -3,3 +3,7 @@ import * as assert from "@eeue56/ts-assert";
 export function testBasicAddition() {
     assert.strictEqual(2, 1 + 1);
 }
+
+export function snapshotOther() {
+    return 1;
+}
